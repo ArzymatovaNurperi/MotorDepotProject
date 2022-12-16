c4a18eb86411402e78f1ad85973c9a6cd1676260
Arzymatova Nurperi <84436333+ArzymatovaNurperi@users.noreply.github.com>
2022-12-16 14:06:35 +0300

    Initial commit

---
+++ src/com/peaksoft/Main.java
@@ -0,0 +1,10 @@
+package com.peaksoft;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+        * Welcome guis it is first project of second stage
+         * good luck*/
+
+    }
+}
